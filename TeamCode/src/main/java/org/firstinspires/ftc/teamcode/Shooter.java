d0378dcfd46a653c1f85de8268daf1c0c0ce5a80
dean_h75 <141525077+deanh75@users.noreply.github.com>
2025-10-31 16:58:44 -0400

    Not using gobilda gyro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -74,9 +74,9 @@ public class Shooter {
         shootRight.setVelocity(1000);
 
         servo.setPosition(0.116);
-        Wait(1250);
+        Wait(1500);
         servo.setPosition(0.187);
-        Wait(2000);
+        Wait(2500);
         servo.setPosition(0.24);
         Wait(750);
         servo.setPosition(0.05);
