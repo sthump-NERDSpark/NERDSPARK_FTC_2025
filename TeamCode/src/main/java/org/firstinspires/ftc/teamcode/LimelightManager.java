2998a127d89f7585cccdf9e45c5a2eee85325e83
sthum <sthump@gmail.com>
2025-11-15 11:10:05 -0500

    RED/BLUE Autons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightManager.java
@@ -77,7 +77,7 @@ public class LimelightManager {
         LLResult result = limelight.getLatestResult();
         pid = new PID(kp, ki, kd);
         if (result.isValid()) {
-            return pid.calculate(result.getTx(), -2);
+            return pid.calculate(result.getTx(), -1);
         }
         return -10;
     }
